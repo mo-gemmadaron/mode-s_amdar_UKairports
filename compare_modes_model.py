36d21c1816e801d71903526ba3e382d7db89c812
mo-gemmadaron <gemma.daron@metoffice.gov.uk>
2022-10-24 16:47:33 +0100

    added code to output csv of means

--- compare_modes_model.py
+++ compare_modes_model.py
@@ -79,18 +79,19 @@ def main():
 
     file_path_netcdf = '/data/users/gdaron/Mode-S_altitude/Min_obs_altd/'
 
-    out_path_plots = '/data/users/gdaron/Mode-S_altitude/Mode-S_day_min/plots'
-    if not os.path.exists(out_path_plots):
-        os.makedirs(out_path_plots)
+    out_path = '/data/users/gdaron/Mode-S_altitude/Mode-S_day_min/outputs'
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
 
 
     # 2. Load NETCDF files
     ds_exist = load_netcdf(file_path_netcdf, 'Existing')
+    ds_priority = load_netcdf(file_path_netcdf, 'Priority')
     ds_all = load_netcdf(file_path_netcdf, 'AllSites')
 
 
     # 3. Select period
-    period = 'summer' # 'summer' or 'winter'
+    period = 'winter' # 'summer' or 'winter'
 
     if period == 'summer':
         start_date = datetime.date(2021,7,10)
@@ -102,8 +103,8 @@ def main():
 
     
     # 4. Select airports
-    airport_name_list = ['Bristol']
-    '''
+    #airport_name_list = ['Bristol', 'Heathrow']
+    
     airport_name_list = ['Heathrow', \
                          'Gatwick', \
                          'Manchester', \
@@ -120,9 +121,10 @@ def main():
                          'LeedsBradford', \
                          'Liverpool',\
                          'Cardiff']
-    '''
+    
+    stats_df = pd.DataFrame() # empty dataframe for stats
 
-    # 5. Loop through airports and extract model data
+    # 5. Loop through airports, extract model data and plot
     for airport in airport_name_list:
 
     	data_day_min = load_day_min(file_path_day_min, airport, period)
@@ -135,28 +137,34 @@ def main():
     		lowest_beam_height_exist, px, py = extract_model_data(ds_exist, point_lon, point_lat, 1000, 1000)
     		data_day_min.loc[idx,'min_obs_altd_exist'] = lowest_beam_height_exist[py, px]
 
+    		# Priority network
+    		lowest_beam_height_priority, px, py = extract_model_data(ds_priority, point_lon, point_lat, 1000, 1000)
+    		data_day_min.loc[idx,'min_obs_altd_priority'] = lowest_beam_height_priority[py, px]
+
     		# AllSites network
     		lowest_beam_height_all, px, py = extract_model_data(ds_all, point_lon, point_lat, 1000, 1000)
     		data_day_min.loc[idx,'min_obs_altd_all'] = lowest_beam_height_all[py, px]
 
     	print(data_day_min)
 
+    	means = data_day_min[['GNSS_ALTD','min_obs_altd_exist','min_obs_altd_priority','min_obs_altd_all']].mean()
+    	stats_df = pd.concat([stats_df, means], axis=1)
 
     	# Plot data (multiple days)
-
     	start_date_reformat = start_date.strftime('%d/%m/%Y')
     	end_date_reformat = end_date.strftime('%d/%m/%Y')
 
     	fig1, ax1 = plt.subplots(figsize=(6,6))
-    	plt.plot(data_day_min['GNSS_ALTD'], marker='.', markersize=3, color ='#1f77b4', linestyle='-', label='Mode-S observed daily minimum')
-    	plt.plot(data_day_min['min_obs_altd_exist'], marker='.', markersize=3, color='#ff7f0e', linestyle='-', label='Model (existing network)')
-    	plt.plot(data_day_min['min_obs_altd_all'], marker='.', markersize=3, color='#2ca02c', linestyle='-', label='Model (full network)')
+
+    	plt.plot(data_day_min['GNSS_ALTD'], marker='.', markersize=3, color ='#1f77b4', linestyle='-', label='Mode-S observed daily minimum', zorder=4)
+    	plt.plot(data_day_min['min_obs_altd_exist'], marker='.', markersize=3, color='#ff7f0e', linestyle='-', label='Model (existing network)', zorder=3)
+    	plt.plot(data_day_min['min_obs_altd_priority'], marker='.', markersize=3, color='#2ca02c', linestyle='-', label='Model (priority network)', zorder=2)
+    	plt.plot(data_day_min['min_obs_altd_all'], marker='.', markersize=3, color='#9467bd', linestyle='-', label='Model (full network)', zorder=1)
 
     	ax1.set_title('{0} airport - {1} '.format(airport, period))
     	ax1.set_xlabel('Date')
     	ax1.set_xlim([start_date, end_date])
     	ax1.xaxis.set_major_formatter(dates.DateFormatter('%d/%m/%Y'))
-    	#ax1.xaxis.set_major_locator(dates.DayLocator(interval=7))
     	ax1.set_xticks(np.arange(start_date, end_date, 7))
 
     	ax1.set_ylabel('Altitude / m')
@@ -164,11 +172,16 @@ def main():
 
     	plt.legend(loc='upper right', prop={'size':12}, facecolor='white')
     	ax1.axhline(y=0, xmin=0, xmax=1, linewidth =1, linestyle = '--', color='black')
-    	#ax1.annotate( 'Mean daily min (Mode-S GNSS): ' + str(round(modes_GNSS_mean,0)) , xy = (10,10), xycoords = 'axes points')
     	plt.tight_layout()
-    	plt.savefig(os.path.join(out_path_plots, "{0}_{1}_day_min.jpg".format(airport, period)))
+    	plt.savefig(os.path.join(out_path, "{0}_{1}_day_min.jpg".format(airport, period)))
     	#plt.show() 
     	plt.close(fig1)
+
+
+    # 6. Export airport stats as csv
+    stats_df.columns = airport_name_list
+    stats_df = stats_df.transpose()
+    stats_df.to_csv(os.path.join(out_path, 'Airport_min_mode-s_stats_{0}.csv'.format(period)))
 
 
 if __name__ == '__main__':
