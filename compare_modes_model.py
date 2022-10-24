ce706648dc80348a03052598e8105e007683001a
mo-gemmadaron <gemma.daron@metoffice.gov.uk>
2022-10-24 15:22:43 +0100

    updated with functions

--- compare_modes_model.py
+++ compare_modes_model.py
@@ -22,9 +22,11 @@ import datetime
 from datetime import timedelta
 import netCDF4 as nc
 
-def load_day_min(file_path, airport, period):
 
-    filename = '{0}/{1}_{2}_day_min_BG.csv'.format(file_path, airport, period) 
+
+def load_day_min(file_path_day_min, airport, period):
+
+    filename = '{0}/{1}_{2}_day_min_BG.csv'.format(file_path_day_min, airport, period) 
 
     if not os.path.isfile(filename):
         return -1
@@ -35,27 +37,59 @@ def load_day_min(file_path, airport, period):
 
     data.set_index('TIME', inplace=True)
 
+    return(data)
+
+
+def load_netcdf(file_path_netcdf, scenario):
+
+    filename = '{0}/constant_ng_network_{1}_deriv.nc'.format(file_path_netcdf, scenario) 
+
+    if not os.path.isfile(filename):
+        return -1
+
+    data = nc.Dataset(filename, 'r')
 
     return(data)
 
 
+def extract_model_data(netcdf_file, point_lon, point_lat, xcellsize, ycellsize):
+
+    lat = netcdf_file.variables['latitude'][:]
+    nrows = lat.size
+    min_lat = np.amin(lat)
+    max_lat = np.amax(lat)
+
+    lon = netcdf_file.variables['longitude'][:]
+    ncols = lon.size
+    min_lon = np.amin(lon)
+    max_lon = np.amax(lon)
+
+    px = int((point_lon - min_lon) / xcellsize)
+    py = int((point_lat - min_lat) / ycellsize)
+
+    lowest_beam_height = netcdf_file.variables['lowest_beam_height_amsl'][:]
+
+    return lowest_beam_height, px, py
+
+
 def main():
 
-    # load locations file
-    file_path = '/data/users/gdaron/Mode-S_altitude/Mode-S_day_min'
-    out_path = '/data/users/gdaron/Mode-S_altitude/Mode-S_day_min/plots'
+    # 1. Set paths
+    file_path_day_min = '/data/users/gdaron/Mode-S_altitude/Mode-S_day_min'
 
-    if not os.path.exists(out_path):
-        os.makedirs(out_path)
+    file_path_netcdf = '/data/users/gdaron/Mode-S_altitude/Min_obs_altd/'
 
-    # load NETCDF4 file
-    fn = '/data/users/gdaron/Mode-S_altitude/Min_obs_altd_existing/constant_ng_network_Existing_deriv.nc'
-    ds = nc.Dataset(fn, 'r')
+    out_path_plots = '/data/users/gdaron/Mode-S_altitude/Mode-S_day_min/plots'
+    if not os.path.exists(out_path_plots):
+        os.makedirs(out_path_plots)
 
-    xcellsize = 1000
-    ycellsize = 1000
 
-    # select period
+    # 2. Load NETCDF files
+    ds_exist = load_netcdf(file_path_netcdf, 'Existing')
+    ds_all = load_netcdf(file_path_netcdf, 'AllSites')
+
+
+    # 3. Select period
     period = 'summer' # 'summer' or 'winter'
 
     if period == 'summer':
@@ -65,55 +99,47 @@ def main():
     if period == 'winter':
         start_date = datetime.date(2022,1,1)
         end_date = datetime.date(2022,1,31)
-    
 
-    #airport_name_list = ['Bristol']
     
+    # 4. Select airports
+    airport_name_list = ['Bristol']
+    '''
     airport_name_list = ['Heathrow', \
-                     'Gatwick', \
-                     'Manchester', \
-                     'Stansted', \
-                     'Edinburgh', \
-                     'Birmingham', \
-                     'Bristol', \
-                     'Glasgow', \
-                     'Aberdeen', \
-                     'EastMidlands', \
-                     'LondonCity', \
-                     'BelfastInt', \
-                     'Newcastle', \
-                     'LeedsBradford', \
-                     'Liverpool',\
-                     'Cardiff']
-    
+                         'Gatwick', \
+                         'Manchester', \
+                         'Stansted', \
+                         'Edinburgh', \
+                         'Birmingham', \
+                         'Bristol', \
+                         'Glasgow', \
+                         'Aberdeen', \
+                         'EastMidlands', \
+                         'LondonCity', \
+                         'BelfastInt', \
+                         'Newcastle', \
+                         'LeedsBradford', \
+                         'Liverpool',\
+                         'Cardiff']
+    '''
+
+    # 5. Loop through airports and extract model data
     for airport in airport_name_list:
 
-    	data = load_day_min(file_path, airport, period)
-    	print(data)
+    	data_day_min = load_day_min(file_path_day_min, airport, period)
 
-    	# Loop through locations and extract minimum observable height value
-    	for idx, row in data.iterrows():
+    	for idx, row in data_day_min.iterrows():
     		point_lat = row['LAT']
     		point_lon = row['LON']
 
-    		lat = ds.variables['latitude'][:]
-    		nrows = lat.size
-    		min_lat = np.amin(lat)
-    		max_lat = np.amax(lat)
+    		# Existing network
+    		lowest_beam_height_exist, px, py = extract_model_data(ds_exist, point_lon, point_lat, 1000, 1000)
+    		data_day_min.loc[idx,'min_obs_altd_exist'] = lowest_beam_height_exist[py, px]
 
-    		lon = ds.variables['longitude'][:]
-    		ncols = lon.size
-    		min_lon = np.amin(lon)
-    		max_lon = np.amax(lon)
+    		# AllSites network
+    		lowest_beam_height_all, px, py = extract_model_data(ds_all, point_lon, point_lat, 1000, 1000)
+    		data_day_min.loc[idx,'min_obs_altd_all'] = lowest_beam_height_all[py, px]
 
-    		# Locate pixel
-    		px = int((point_lon - min_lon) / xcellsize)
-    		py = int((point_lat - min_lat) / ycellsize)
-
-    		lowest_beam_height_amsl = ds.variables['lowest_beam_height_amsl'][:]
-    		data.loc[idx,'min_obs_altd'] = lowest_beam_height_amsl[py, px]
-
-    	print(data)
+    	print(data_day_min)
 
 
     	# Plot data (multiple days)
@@ -122,8 +148,9 @@ def main():
     	end_date_reformat = end_date.strftime('%d/%m/%Y')
 
     	fig1, ax1 = plt.subplots(figsize=(6,6))
-    	plt.plot(data['GNSS_ALTD'], marker='.', markersize=3, color ='#1f77b4', linestyle='-', label='Mode-S observed daily minimum')
-    	plt.plot(data['min_obs_altd'], marker='.', markersize=3, color='#d62728', linestyle='-', label='Model')
+    	plt.plot(data_day_min['GNSS_ALTD'], marker='.', markersize=3, color ='#1f77b4', linestyle='-', label='Mode-S observed daily minimum')
+    	plt.plot(data_day_min['min_obs_altd_exist'], marker='.', markersize=3, color='#ff7f0e', linestyle='-', label='Model (existing network)')
+    	plt.plot(data_day_min['min_obs_altd_all'], marker='.', markersize=3, color='#2ca02c', linestyle='-', label='Model (full network)')
 
     	ax1.set_title('{0} airport - {1} '.format(airport, period))
     	ax1.set_xlabel('Date')
@@ -139,56 +166,10 @@ def main():
     	ax1.axhline(y=0, xmin=0, xmax=1, linewidth =1, linestyle = '--', color='black')
     	#ax1.annotate( 'Mean daily min (Mode-S GNSS): ' + str(round(modes_GNSS_mean,0)) , xy = (10,10), xycoords = 'axes points')
     	plt.tight_layout()
-    	plt.savefig(os.path.join(out_path, "{0}_{1}_day_min.jpg".format(airport, period)))
+    	plt.savefig(os.path.join(out_path_plots, "{0}_{1}_day_min.jpg".format(airport, period)))
     	#plt.show() 
     	plt.close(fig1)
-'''
 
-    fn = '/data/users/gdaron/Mode-S_altitude/Min_obs_altd_existing/constant_ng_network_Existing_deriv.nc'
-    ds = nc.Dataset(fn, 'r')
-
-    point_lat = 179887
-    point_lon = 542388
-
-    #print(ds)
-    #print(ds.__dict__)
-
-    #for dim in ds.dimensions.values():
-    	#print(dim)
-
-    #for var in ds.variables.values():
-    	#print(var)
-
-    #print(ds['lowest_beam_height_amsl'])
-
-    lat = ds.variables['latitude'][:]
-    nrows = lat.size
-    min_lat = np.amin(lat)
-    max_lat = np.amax(lat)
-
-    lon = ds.variables['longitude'][:]
-    ncols = lon.size
-    min_lon = np.amin(lon)
-    max_lon = np.amax(lon)
-
-    #xcellsize = (max_lon - min_lon) / ncols
-    #ycellsize = (max_lat - min_lat) / nrows
-    xcellsize = 1000
-    ycellsize = 1000
-    #print(xcellsize)
-    #print(ycellsize)
-    px = int((point_lon - min_lon) / xcellsize)
-    py = int((point_lat - min_lat) / ycellsize)
-    print(px)
-    print(py)
-
-
-    lowest_beam_height_amsl = ds.variables['lowest_beam_height_amsl'][:]
-    #print(lowest_beam_height_amsl)
-    #print(lowest_beam_height_amsl.size)
-    print(lowest_beam_height_amsl[py, px]) # latitude, longitude
-
-'''
 
 if __name__ == '__main__':
     main()
